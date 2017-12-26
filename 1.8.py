7d9bb9a229eab6aaf43ca4092440d013da7ba55c
Alexander Nazarovsky <ANazarovsky@gmail.com>
2017-12-26 18:55:03 +0300

    Decomposition of code

--- 1.8.py
+++ 1.8.py
@@ -13,17 +13,19 @@ def report(file_name, type):
         for i in answer:
             print("Слово:", i[0], "\nКоличество упоминаний в тексте: ", i[1])
 
-
-while True:
-    console = input("Choose a file to open:\n1-France\n2-Africa\n3-Cyprus\n4-Italy\nQ-for quit")
-    if console == "1":
-        report("newsfr.txt", None)
-    elif console == "2":
-        report("newsafr.txt", "utf-8")
-    elif console == "3":
-        report("newscy.txt", None)
-    elif console == "4":
-        report("newsit.txt", None)
-    elif console.lower() == "q":
-        break
+def main():
+    while True:
+        console = input("Choose a file to open:\n1-France\n2-Africa\n3-Cyprus\n4-Italy\nQ-for quit")
+        if console == "1":
+            report("newsfr.txt", None)
+        elif console == "2":
+            report("newsafr.txt", "utf-8")
+        elif console == "3":
+            report("newscy.txt", None)
+        elif console == "4":
+            report("newsit.txt", None)
+        elif console.lower() == "q":
+            break
 os.system("pause")
+
+main()
