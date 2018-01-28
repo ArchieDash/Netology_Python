2e7f9f7e5e8eb0443223a95348d973c64948c658
Alexander Nazarovsky <ANazarovsky@gmail.com>
2018-01-28 21:31:43 +0300

    PY11-Home_Work_2.5

--- 2.5.py
+++ 2.5.py
@@ -3,10 +3,21 @@ import subprocess
 import queue
 
 if __name__ == "__main__":
-    directory = os.path.abspath(os.path.dirname(__file__))
-    images = os.listdir(os.path.join(directory, "Source"))
-    q = queue.Queue()
-    for img in images:
-        q.put(img)
-        subprocess.run("convert.exe Source/{0} -resize 200 Results/{0}".format(q.get()))
-    pass
+
+    def resize(images):
+        q = queue.Queue()
+        for img in images:
+            q.put(img)
+            subprocess.run("convert.exe Source/{0} -resize 200 Results/{0}".format(q.get()))
+        pass
+
+    def main():
+        directory = os.path.abspath(os.path.dirname(__file__))
+        images = os.listdir(os.path.join(directory, "Source"))
+        try:
+            os.mkdir(os.path.join(directory, "Results"))
+        except:
+            pass
+        resize(images)
+
+    main()
