eba99fbe4d074a6ef3b35fa668721df9fc502679
Archie Dash <ANazarovsky@gmail.com>
2018-03-24 11:47:38 +0300

    PY-11 Diploma

--- Diploma.py
+++ Diploma.py
@@ -3,21 +3,29 @@ import time
 import json
 
 
-def main():
-    token = "7b23e40ad10e08d3b7a8ec0956f2c57910c455e886b480b7d9fb59859870658c4a0b8fdc4dd494db19099"
-
+def user_name():
     user = input("User ID or name: ")
     if user.isdigit():
         user_id = user
     else:
         params = {"screen_name": user, "version": "5.73"}
         user_id = requests.get("https://api.vk.com/method/utils.resolveScreenName", params).json()["response"]["object_id"]
+    return user_id
 
+
+def find_friends(token, user_id):
     params = {"access_token": token, "user_id": user_id, "version": "5.73"}
     friends = requests.get("https://api.vk.com/method/friends.get", params).json()["response"]
+    return friends
+
+
+def find_groups(token, user_id):
     params = {"access_token": token, "user_id": user_id, "version": "5.73", "count": "1000"}
     groups = requests.get("https://api.vk.com/method/groups.get", params).json()["response"][1:]
+    return groups
 
+
+def groups_info(token, friends, groups):
     progress = 1
     print("Getting information about groups...")
     for friend in friends:
@@ -30,7 +38,10 @@ def main():
         print(f"{progress} / {len(friends)} - {round(progress*100/len(friends), 2)} %")
         progress += 1
         groups = set(groups) - set(f_groups)
+    return groups
 
+
+def target_groups(groups):
     progress = 1
     print("Formatting results...")
     group_description = list()
@@ -42,9 +53,19 @@ def main():
         progress += 1
         group_info = dict(zip(["name", "gid", "members_count"],[info[0]["name"], info[0]["gid"], info[0]["members_count"]]))
         group_description.append(group_info)
+    return group_description
 
+
+def result(group_description):
     with open("groups.json", "w", encoding="utf8") as f:
         json.dump(group_description, f, ensure_ascii=False)
 
 
-main()
+def main():
+    token = "7b23e40ad10e08d3b7a8ec0956f2c57910c455e886b480b7d9fb59859870658c4a0b8fdc4dd494db19099"
+    user = user_name()
+    result(target_groups(groups_info(token, find_friends(token, user), find_groups(token, user))))
+
+
+if __name__ == "__main__":
+    main()
