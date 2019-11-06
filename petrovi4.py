8039f00a50fb9225fd70f27c4ca29a24546e3560
Alexander Nazarovsky <ANazarovsky@gmail.com>
2019-11-06 16:35:07 +0300

    1.2 ExtraTask (Petrovi4's saplings)

---
+++ petrovi4.py
@@ -0,0 +1,29 @@
+import os
+
+
+def sapling(shift):
+    print("Выкапываем яму")
+    if shift == "НЕТ":
+        print("Петрович кладет саженец")
+    print("Серега закапывает яму")
+    print("Перекур")
+
+
+def main():    
+    print("САЖАЕМ САЖЕНЦЫ\n\n")
+    while (True):
+        shift = input("Петрович опять болеет?\n").upper()
+        if shift == "НЕТ" or shift == "ДА":
+          break
+    sapling(shift)
+    while (True):
+        answer = input("Рабочая смена закончена?").upper()
+        if answer == "ДА":
+            break
+        elif answer == "НЕТ":
+            sapling(shift)
+    os.system("pause")
+
+
+if __name__ == "__main__":
+    main()
