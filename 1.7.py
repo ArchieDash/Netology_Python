e3022aa2deafc78d2e5de38d4c7c900e33051786
Alexander Nazarovsky <ANazarovsky@gmail.com>
2019-11-06 16:04:04 +0300

    1.7 (Farm - Classes exercise)

--- 1.7.py
+++ 1.7.py
@@ -1,113 +1,104 @@
 class Animals:
     lifespan = 0
-
-
-class Cattle(Animals):
+  
+class Cattle (Animals):
     reproduction = "live birth"
     legs = 4
+    food = ("hay", "greenery", "roots", "cereals")
 
-
-class Birds(Animals):
+class Birds (Animals):
     reproduction = "eggs"
     wings = True
+    food = ("greenery", "roots", "cereals")
 
-
-class Cow(Cattle):
+class Cow (Cattle):
     sound = "moo-moo"
     lifespan = 20
     horns = True
-    food = ("hay", "greenery")
+    food = Cattle.food[0], Cattle.food[1]
 
-
-class Goat(Cattle):
+class Goat (Cattle):
     sound = "baa"
     lifespan = 16.5
     horns = True
-    food = ("hay", "greenery")
+    food = Cattle.food[0], Cattle.food[1]
 
-
-class Sheep(Cattle):
+class Sheep (Cattle):
     sound = "baa-baa"
     lifespan = 11
     horns = False
-    food = ("hay", "greenery")
+    food = Cattle.food[0], Cattle.food[1]
 
-
-class Pig(Cattle):
+class Pig (Cattle):
     sound = "grunt-grunt"
     lifespan = 20
     horns = False
-    food = ("roots", "cereals")
+    food = Cattle.food[2], Cattle.food[3]
 
-
-class Duck(Birds):
+class Duck (Birds):
     sound = "quack-quack"
     lifespan = 7.5
-    food = ("greenery", "cereals")
+    food = Birds.food[0], Birds.food[2]
 
-
-class Chicken(Birds):
+class Chicken (Birds):
     sound = "cheep-cheep"
     lifespan = 4
-    food = "cereals"
-
-
-class Goose(Birds):
+    food = Birds.food[2]
+  
+class Goose (Birds):
     sound = "honk-honk"
     lifespan = 20
-    food = ("greenery", "roots")
-
-
-def report(console, specimen):
+    food = Birds.food[0], Birds.food[1]
+    
+    
+def report (console, specimen):
     try:
-        if specimen.legs == 4:
-            print ("As all cattle", console, "walks on 4 legs")
+      if specimen.legs == 4:
+        print ("As all cattle", console, "walks on 4 legs")
     except:
-        None
+      None
     try:
-        if specimen.wings == True:
-            print ("All birds has wings. And", console, "too.")
+      if specimen.wings == True:
+        print ("All birds has wings. And", console, "too.")
     except:
-        None
+      None
     print ("It is sounds:", specimen.sound)
     print ("Average lifespan is {} years".format(specimen.lifespan))
     if specimen.reproduction == "live birth":
-        print ("Cattle are mammals.\nAnd all mammals are {} :3".format(specimen.reproduction))
+      print ("Cattle are mammals.\nAnd all mammals are {} :3".format(specimen.reproduction))
     elif specimen.reproduction == "eggs":
-        print ("Take care about their {}.\nIt is their children ^^".format(specimen.reproduction))
+      print ("Take care about their {}.\nIt is their children ^^".format(specimen.reproduction))
     if type(specimen.food) is tuple:
-        print ("Ration: {}".format(", ".join(specimen.food)))
+      print ("Ration: {}". format (", ".join(specimen.food)))
     else:
-        print ("Ration: {}".format(specimen.food))
+      print ("Ration: {}".format (specimen.food))
     try:
-        if specimen.horns is True:
-            print ("And it has horns!")
+      if specimen.horns is True:
+        print ("And it has horns!")
     except:
-        return None
-
-
-def main():
-    print ("Choose an animal at you farm. You have: cow, sheep, goat, pig, chicken, goose and duck.")
+      return None
+      
+def main ():
+    print ("Choose an animal at you farm. You have: cow, sheep, goat, pig, chicken, goose and duck.")  
     while True:
-        console = input("\nYou have take a closer look at:").lower()
-        if console == "cow":
-            report(console, Cow())
-        elif console == "sheep":
-            report(console, Sheep())
-        elif console == "pig":
-            report(console, Pig())
-        elif console == "goat":
-            report(console, Goat())
-        elif console == "chicken".lower():
-            report(console, Chicken())
-        elif console == "goose".lower():
-            report(console, Goose())
-        elif console == "duck".lower():
-            report(console, Duck())
-        elif console == "q":
-            break
-        else:
-            print ("You don't have such animal at farm")
-
-
-main()
+      console = input("\nYou have take a closer look at:").lower()
+      if console == "cow":
+        report(console, Cow())
+      elif console == "sheep":
+        report(console, Sheep())
+      elif console == "pig":
+        report(console, Pig())
+      elif console == "goat":
+        report(console, Goat())
+      elif console == "chicken".lower():
+        report(console, Chicken())
+      elif console == "goose".lower():
+        report(console, Goose())
+      elif console == "duck".lower():
+        report(console, Duck())
+      elif console == "q":
+        break
+      else:
+        print ("You don't have such animal at farm")
+      
+main ()
